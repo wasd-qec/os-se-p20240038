83d2f199cd1bd51caefefd51659cbff67d90304b
RithChankolboth <rithchankolboth@gmail.com>
2026-04-01 17:09:05 +0700

    Completed Lab 3 — Wildcards, Links, GRUB & Shared Libraries

---
+++ os-lab-p20240038/lab3/shared_lib_lab/techcorp_sysinfo.h
@@ -0,0 +1,11 @@
+#ifndef TECHCORP_SYSINFO_H
+#define TECHCORP_SYSINFO_H
+
+const char* tc_get_hostname(void);
+const char* tc_get_uptime(void);
+int tc_get_cpu_count(void);
+
+// NEW — Returns total memory in MB
+long tc_get_memory_mb(void);
+
+#endif
